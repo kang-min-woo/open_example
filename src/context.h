51b1b9d97b559f885a38d03fd65038ef2cca8ff8
kangmin-woo <mincuz@naver.com>
2021-04-16 21:05:50 +0900

    week_07 imgui

--- src/context.h
+++ src/context.h
@@ -6,12 +6,18 @@
 #include "program.h"
 #include "buffer.h"
 #include "vertex_layout.h"
+#include "texture.h"
 
 CLASS_PTR(Context)
 class Context {
 public:
     static ContextUPtr Create();
-    void Render();    
+    void Render();
+    void ProcessInput(GLFWwindow* window);    
+    void Reshape(int width, int height);
+    void MouseMove(double x, double y);
+    void MouseButton(int button, int action, double x, double y);
+    
 private:
     Context() {}
     bool Init();
@@ -20,7 +26,24 @@ private:
     VertexLayoutUPtr m_vertexLayout;
     BufferUPtr m_vertexBuffer;
     BufferUPtr m_indexBuffer;
+    TextureUPtr m_texture;
+    TextureUPtr m_texture2;
 
+    // clear color
+    glm::vec4 m_clearColor { glm::vec4(0.7f, 0.8f, 0.9f, 0.0f) };
+
+    // camera parameter
+    bool m_cameraControl { false };
+    glm::vec2 m_prevMousePos { glm::vec2(0.0f) };
+    float m_cameraPitch { 0.0f };
+    float m_cameraYaw { 0.0f };
+    glm::vec3 m_cameraPos { glm::vec3(0.0f, 0.0f, 3.0f) };
+    glm::vec3 m_cameraFront { glm::vec3(0.0f, 0.0f, -1.0f) };
+    glm::vec3 m_cameraUp { glm::vec3(0.0f, 1.0f, 0.0f) };
+
+    	
+    int m_width { WINDOW_WIDTH };
+    int m_height { WINDOW_HEIGHT };
 };
 
 #endif // __CONTEXT_H__
